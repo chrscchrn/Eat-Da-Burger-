33296d293d86b11cc7f8be9245e5734a9f678652
chrscchrn <cochranschris@gmail.com>
2020-09-11 21:25:47 -0500

    heroku troubleshooting

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,17 @@ var connection = mysql.createConnection({
     database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'w1h4cr5sb73o944p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'vh3vc060qiay8c1e',
+        password: 'bjd8mdwzufznz0dq',
+        database: 'gqxtajze4a04vqqq'
+    })
+}
+
 connection.connect((err) => {
     if (err) {
         console.error("error connecting: " + err.stack);
